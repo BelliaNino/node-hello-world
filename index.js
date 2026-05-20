9125c49b4fdd50e0cf405b893623fae7ce254da0
BelliaNino <bellianino1996@gmail.com>
2026-05-20 16:27:41 +0200

    superbonus

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+import { cowsay } from "cowsayjs";
+
 console.log('hello booleano');
 console.log(process.argv);
 
 const otherword = process.argv[4];
-console.log(otherword);
+
+console.log(cowsay(otherword));
+
+
 
